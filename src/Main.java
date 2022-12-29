19b7c8f41d474fd6960ffab7a50b9d473396d81d
ilia0312 <stashkov.ilia0312@yandex.ru>
2022-12-29 17:39:36 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        int clientOS = 1;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        }
+
+
+    public static void task2() {
+        System.out.println("Задача 2");
+
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+    }
+}
